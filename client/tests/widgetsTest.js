5f53d7adefd78de5f74ee2124c9fe55c5ab9ae66
Pernille Kopperud <pernilko@stud.ntnu.no>
2019-11-21 16:37:37 +0100

    flow

---
+++ client/tests/widgetsTest.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import { Alert } from '../src/widgets.js';
+import { shallow, mount } from 'enzyme';
+
+describe('Alert tests', () => {
+    const wrapper = shallow(<Alert />);
+
+    it('initially', () => {
+        let instance = Alert.instance();
+        expect(typeof instance).toEqual('object');
+        if (instance) expect(instance.alerts).toEqual([]);
+
+        expect(wrapper.find('button.close')).toHaveLength(0);
+    });
+
+    it('after danger', done => {
+        Alert.danger('test');
+
+        // Wait for the Alert component to finish drawing
+        setTimeout(() => {
+            let instance = Alert.instance();
+            expect(typeof instance).toEqual('object');
+            if (instance) expect(instance.alerts).toEqual([{ id: 0, text: 'test', type: 'danger' }]);
+
+            expect(wrapper.find('button.close')).toHaveLength(1);
+
+            done();
+        });
+    });
+
+    it('after clicking close button', () => {
+        wrapper.find('button.close').simulate('click');
+
+        let instance = Alert.instance();
+        expect(typeof instance).toEqual('object');
+        if (instance) expect(instance.alerts).toEqual([]);
+
+        expect(wrapper.find('button.close')).toHaveLength(0);
+    });
+});
