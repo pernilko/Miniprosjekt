d8cbc40df1595e26803e4710e0d141c1684fdf56
Pernille Kopperud <pernilko@stud.ntnu.no>
2019-11-22 14:18:34 +0100

    Client test done

---
+++ client/tests/articlepageTest.js
@@ -0,0 +1,32 @@
+// @flow
+import * as React from 'react';
+import { ArticlePage } from '../src/ArticlePage.js';
+import { shallow, mount } from 'enzyme';
+import {Article, articleStore} from "../src/stores";
+
+describe('NewsFeed test', () => {
+    const wrapper = shallow(<ArticlePage match={{params: {id: 1}}}/>);
+
+
+    it('initially', () => {
+        let instance = ArticlePage.instance();
+        expect(typeof instance).toEqual('object');
+        jest.spyOn(articleStore, 'getArticle').mockResolvedValue([]);
+        wrapper.update();
+        if (instance) expect(wrapper.debug()).toMatchSnapshot();
+    });
+
+    it('after load', () => {
+        let article: Article = [new Article(1,'Ola Nordmann', 'header', 'content', '22.11.2019 02:20',
+            'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', 'Sport', 2,0)];
+        jest.spyOn(articleStore, 'getArticle').mockResolvedValue(article);
+        wrapper.update();
+        let instance = ArticlePage.instance();
+        expect(typeof instance).toEqual('object');
+        if (instance) {
+            instance.forceUpdate();
+            articleStore.currentArticle = article;
+            expect(wrapper.debug()).toMatchSnapshot();
+        }
+    });
+});
---
+++ client/tests/newsfeedTest.js
@@ -0,0 +1,32 @@
+// @flow
+import * as React from 'react';
+import { NewsFeed } from '../src/Feed.js';
+import { shallow, mount } from 'enzyme';
+import {Article, articleStore} from "../src/stores";
+
+describe('NewsFeed test', () => {
+    const wrapper = shallow(<NewsFeed/>);
+
+
+    it('initially', () => {
+        let instance = NewsFeed.instance();
+        expect(typeof instance).toEqual('object');
+        jest.spyOn(articleStore, 'getArticles').mockResolvedValue([]);
+        wrapper.update();
+        if (instance) expect(wrapper.debug()).toMatchSnapshot();
+    });
+
+    it('after load', () => {
+        let articles: Article[] = [new Article(1,'Ola Nordmann', 'header', 'content', '22.11.2019 02:20',
+            'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80', 'Sport', 2,0)];
+        jest.spyOn(articleStore, 'getArticles').mockResolvedValue(articles);
+        wrapper.update();
+        let instance = NewsFeed.instance();
+        expect(typeof instance).toEqual('object');
+        if (instance) {
+            instance.forceUpdate();
+            articleStore.articles = articles;
+            expect(wrapper.debug()).toMatchSnapshot();
+        };
+    });
+});
